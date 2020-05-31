cfb31acb2da8b22ba9cbda13299ff8d3770322f3
10nayan <44505856+10nayan@users.noreply.github.com>
2020-05-31 14:56:09 +0530

    chat functionality added

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,21 +1,28 @@
 document.addEventListener('DOMContentLoaded',()=>{
 var socket = io();
 socket.on('connect',()=> {
-socket.send("I am connected")
+socket.send(firstName)
 });
 socket.on('message',data=>{
-  console.log(`Message received :${data}`)
+  console.log(`${data} is connected`)
 });
 socket.on('my_event',data=>{
   console.log(data)
 });
 document.querySelector('#submit').onclick=()=>{
-  socket.emit('new_event',{'msg':document.querySelector('#input').value,'name':firstName});
+  var mesg=document.querySelector('#input').value;
+  var rcpnt=document.querySelector('#receiver').value;
+  console.log(rcpnt);
+  socket.emit('new_event',{'msg':mesg,'name':firstName,'receiver':rcpnt});
+  const q=document.createElement('p');
+  const dr=document.createElement('br');
+  q.innerHTML=firstName+' '+mesg;
+  document.querySelector('#messages').append(q);
 };
 socket.on('new_response',data=>{
   const p=document.createElement('p');
   const br=document.createElement('br');
-  p.innerHTML=data.time+data.name+data.msg;
+  p.innerHTML=data.time+' '+data.name+' '+data.msg;
   document.querySelector('#messages').append(p);
 
 });
