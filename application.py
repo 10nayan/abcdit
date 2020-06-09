b413af927fbc95bf5d80c2dd14aa4ff3173dc79f
10nayan <44505856+10nayan@users.noreply.github.com>
2020-06-09 16:18:13 +0530

    new feature updated

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,request,url_for,redirect,session,flash,j
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager,login_user,logout_user,login_required,current_user
 from flask_session import Session
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from datetime import datetime,date
 from passlib.hash import pbkdf2_sha256
 from models import *
@@ -15,6 +17,9 @@ app.config["SESSION_PERMANENT"]=False
 app.config["SESSION_TYPE"]="filesystem"
 app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:Nayan@123.@localhost/postgres"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =False
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+admin = Admin(app, name='admin-page', template_mode='bootstrap3')
+admin.add_view(ModelView(User, db.session))
 Session(app)
 socketio = SocketIO(app)
 notes=[]
@@ -26,6 +31,11 @@ login.init_app(app)
 @login.user_loader
 def user_loader(id):
     return User.query.get(int(id))
+def validate_email(email):
+    if email[-4::]=='.com' or email[-3::]=='.in':
+        return True
+    else:
+        return False
 @app.route("/home")
 def home():
     if current_user.is_active:
@@ -45,10 +55,21 @@ def signup():
         gender=request.form.get("gender")
         birthday=request.form.get("birthday")
         password=request.form.get("password")
+        password2=request.form.get("password2")
         hashed=pbkdf2_sha256.hash(password)
+        if User.query.filter_by(email=email).first():
+            flash(u"Email already exists","danger")
+            return redirect(url_for('signup'))
+        if password!=password2:
+            flash(u"Both password should be same","warning")
+            return redirect(url_for('signup'))
+        if not validate_email(email):
+            flash(u"Email not valid","error")
+            return redirect(url_for('signup'))
         user=User(first_name=first_name,last_name=last_name,email=email,gender=gender,birthday=birthday,hash=hashed)
         db.session.add(user)
         db.session.commit()
+        flash(u"You have registered successfully","success")
         return redirect(url_for('signin'))
     return render_template("signup.html")
 @app.route("/signin",methods=["POST","GET"])
@@ -58,53 +79,23 @@ def signin():
         password=request.form.get("password")
         user=User.query.filter_by(email=email).first()
         if current_user.is_active:
-            flash("You are already logged in, logged out first")
+            flash(u"You are already logged in","warning")
             return redirect(url_for('signin'))
         if not user:
-            flash('User not found')
+            flash(u'User not found','error')
             return redirect(url_for('signin'))
         if not pbkdf2_sha256.verify(password,user.hash):
-            flash("Incorrect password")
+            flash(u"Incorrect password","danger")
             return redirect(url_for('signin'))
         login_user(user)
         return redirect(url_for('index'))
     return render_template("signin.html")
-@app.route("/more",methods=["POST","GET"])
-def more():
-	if session.get ("notes") is None:
-		session["notes"]=[]
-	if request.method=="POST":
-		note=request.form.get("note")
-		session["notes"].append(note)
-	return render_template("more.html",notes=session["notes"],user=current_user)
-@app.route("/converts",methods=["POST","GET"])
-def converts():
-    if request.method=="POST":
-        other=request.form.get('other')
-        res=requests.get("http://data.fixer.io/api/latest?access_key=5c0328116134b8ff2be7cb3103709f89&format=1",params={'symbols':other})
-        if res.status_code!=200:
-            raise Exception("Error :API request unsuccessful")
-        data=res.json()
-        rate=data['rates'][other]
-        return f'1 EURO is equal to {rate} {other}'
-    return '''<form method="POST">
-        Enter The Currency Symbol to where you want to convert: <input type="text" name="other">
-        <input type="submit">
-        </form>'''
-@app.route("/currency")
-def currency():
-    return render_template("currency.html")
-@app.route("/convert",methods=["POST"])
+@app.route("/calender",methods=["POST","GET"])
+def calender():
+	return render_template("calender.html")
+@app.route("/convert")
 def convert():
-    currency = request.form.get("currency")
-
-    res=requests.get("http://data.fixer.io/api/latest?access_key=5c0328116134b8ff2be7cb3103709f89&format=1",params={'symbols':currency})
-    if res.status_code != 200:
-        return jsonify({"success": False})
-    data = res.json()
-    if currency not in data["rates"]:
-        return jsonify({"success": False})
-    return jsonify({"success": True, "rate": data["rates"][currency]})
+    return render_template("convert.html")
 @app.route("/weather")
 def weather():
     return render_template("weather.html")
@@ -125,79 +116,108 @@ def about():
 @login_required
 def signout():
     logout_user()
-    flash("You have logged out")
+    flash(u"You have logged out","success")
     return redirect(url_for('signin'))
 @app.route("/calculate",methods=["GET"])
 def calculate():
     return render_template("calculate.html",data=data)
 @app.route("/calculate/quadratic",methods=["POST"])
 def quadratic():
-    a=request.form.get('a')
-    b=request.form.get('b')
-    c=request.form.get('c')
-    data['results1']=np.roots([a,b,c])
+    try:
+        a=request.form.get('a')
+        b=request.form.get('b')
+        c=request.form.get('c')
+        data['results1']=np.roots([a,b,c])
+    except:
+        data['results1']="Error, either a=0 or some input field left empty"
     return render_template("calculate.html",data=data)
 @app.route("/calculate/linear",methods=["POST"])
 def Linear():
-    a1=request.form.get('a1')
-    b1=request.form.get('b1')
-    c1=request.form.get('c1')
-    a2=request.form.get('a2')
-    b2=request.form.get('b2')
-    c2=request.form.get('c2')
-    A=np.array([[a1,b1],[a2,b2]],dtype='float')
-    b=np.array([c1,c2],dtype='float')
-    data['results2']=numpy.linalg.solve(A,b)
+    try:
+        a1=request.form.get('a1')
+        b1=request.form.get('b1')
+        c1=request.form.get('c1')
+        a2=request.form.get('a2')
+        b2=request.form.get('b2')
+        c2=request.form.get('c2')
+        A=np.array([[a1,b1],[a2,b2]],dtype='float')
+        b=np.array([c1,c2],dtype='float')
+        data['results2']=numpy.linalg.solve(A,b)
+    except:
+        data['results2']="Error in calulation or some input field left empty"
     return render_template("calculate.html",data=data)
 @app.route("/calculate/integrate",methods=["POST"])
 def integrate():
-    a2=request.form.get('a2')
-    b2=request.form.get('b2')
-    c2=request.form.get('c2')
-    d2=request.form.get('d2')
-    e2=request.form.get('e2')
-    f2=request.form.get('f2')
-    array=np.array([a2,b2,c2,d2,e2,f2],dtype='float')
-    data['results3']=np.polyint(array)
+    try:
+        a2=request.form.get('a2')
+        b2=request.form.get('b2')
+        c2=request.form.get('c2')
+        d2=request.form.get('d2')
+        e2=request.form.get('e2')
+        f2=request.form.get('f2')
+        array=np.array([a2,b2,c2,d2,e2,f2],dtype='float')
+        data['results3']=np.polyint(array)
+    except:
+        data['results3']="Error in calulation or some input field left empty"
     return render_template("calculate.html",data=data)
 @app.route("/calculate/differenciate",methods=["POST"])
 def differenciate():
-    a3=request.form.get('a3')
-    b3=request.form.get('b3')
-    c3=request.form.get('c3')
-    d3=request.form.get('d3')
-    e3=request.form.get('e3')
-    f3=request.form.get('f3')
-    array=np.array([a3,b3,c3,d3,e3,f3],dtype='float')
-    data['results4']=np.polyder(array)
+    try:
+        a3=request.form.get('a3')
+        b3=request.form.get('b3')
+        c3=request.form.get('c3')
+        d3=request.form.get('d3')
+        e3=request.form.get('e3')
+        f3=request.form.get('f3')
+        array=np.array([a3,b3,c3,d3,e3,f3],dtype='float')
+        data['results4']=np.polyder(array)
+    except:
+        data['results4']="Error in calulation or some input field left empty"
     return render_template("calculate.html",data=data)
 @app.route("/calculate/evaluate",methods=["POST"])
 def evaluate():
-    a4=request.form.get('a4')
-    b4=request.form.get('b4')
-    c4=request.form.get('c4')
-    d4=request.form.get('d4')
-    e4=request.form.get('e4')
-    x=request.form.get('x')
-    array=np.array([a4,b4,c4,d4,e4],dtype='float')
-    data['results5']=np.polyval(array,int(x))
+    try:
+        a4=request.form.get('a4')
+        b4=request.form.get('b4')
+        c4=request.form.get('c4')
+        d4=request.form.get('d4')
+        e4=request.form.get('e4')
+        x=request.form.get('x')
+        array=np.array([a4,b4,c4,d4,e4],dtype='float')
+        data['results5']=np.polyval(array,int(x))
+    except:
+        data['results5']="Error in calulation or some input field left empty"
     return render_template("calculate.html",data=data)
 @app.route("/chat",methods=["GET","POST"])
 def chat():
     if not current_user.is_authenticated:
-        flash("You have to login first")
+        flash(u"You have to login first","error")
         return redirect(url_for('signin'))
     name=current_user.first_name
-    return render_template("chat.html",name=name)
+    email=current_user.email
+    return render_template("chat.html",name=name,email=email)
 @socketio.on('message')
-def handle_message(username):
-    users[username]=request.sid
-    send(username)
+def handle_message(email):
+    users[email]=request.sid
+    send(email)
     emit('my_event','This task is also completed')
 @socketio.on('new_event')
 def handle_newevent(message):
-    receiver_session_id=users[message['receiver']]
-    message['time']=datetime.now().strftime("%I:%M %p")
-    emit('new_response',message,room=receiver_session_id)
+    try:
+        receiver_session_id=users[message['receiver']]
+        message['time']=datetime.now().strftime("%I:%M %p")
+        emit('new_response',message,room=receiver_session_id)
+        print(users)
+    except:
+        message['name']="Server"
+        message['msg']=message['receiver']+"  has left or not available"
+        message['time']=datetime.now().strftime("%I:%M %p")
+        emit('new_response',message)
+        print(users)
+@login_required
+@app.route("/chat/leave")
+def leave():
+    users.pop(current_user.email,None)
+    return  redirect(url_for('index'))
 if __name__ == '__main__':
-	socketio.run(app)
+	socketio.run(app,debug=True)
